a6b480934bb41ee6b4f508d3b76afe266add9dcb
chaikebin <chkeb@126.com>
2023-07-17 18:00:25 +0800

    update

--- clients/chatgpt/chatgpt.go
+++ clients/chatgpt/chatgpt.go
@@ -26,38 +26,7 @@ func (c *ChatGPT) Chat(prompt string) (string, error) {
 	req := ChatGPTRequest{
 		Prompt: prompt,
 	}
-	reqBytes, err := json.Marshal(req)
-	if err != nil {
-		return "", err
-	}
-
-	// 2. 发送请求
-	url := "https://api.openai.com/v1/engines/davinci/completions"
-	reqReader := bytes.NewReader(reqBytes)
-	httpReq, err := http.NewRequest("POST", url, reqReader)
-	if err != nil {
-		return "", err
-	}
-	httpReq.Header.Set("Content-Type", "application/json")
-	httpReq.Header.Set("Authorization", "Bearer "+c.APIKey)
-	httpClient := http.Client{}
-	resp, err := httpClient.Do(httpReq)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	// 3. 解析响应
-	respBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	var respData ChatGPTResponse
-	err = json.Unmarshal(respBytes, &respData)
-	if err != nil {
-		return "", err
-	}
-	return respData.Choices[0].Text, nil
+	return c.request(req)
 }
 
 func (c *ChatGPT) ChatWithHistory(prompt string, history []string) (string, error) {
@@ -68,75 +37,40 @@ func (c *ChatGPT) ChatWithHistory(prompt string, history []string) (string, erro
 	for _, h := range history {
 		req.Prompt += "\nHuman: " + h
 	}
-	reqBytes, err := json.Marshal(req)
-	if err != nil {
-		return "", err
-	}
-
-	// 2. 发送请求
-	url := "https://api.openai.com/v1/engines/davinci/completions"
-	reqReader := bytes.NewReader(reqBytes)
-	httpReq, err := http.NewRequest("POST", url, reqReader)
-	if err != nil {
-		return "", err
-	}
-	httpReq.Header.Set("Content-Type", "application/json")
-	httpReq.Header.Set("Authorization", "Bearer "+c.APIKey)
-	httpClient := http.Client{}
-	resp, err := httpClient.Do(httpReq)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	// 3. 解析响应
-	respBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	var respData ChatGPTResponse
-	err = json.Unmarshal(respBytes, &respData)
-	if err != nil {
-		return "", err
-	}
-	return respData.Choices[0].Text, nil
+	return c.request(req)
 }
 
-func (c *ChatGPT) ChatWithTemperature(prompt string, temperature float64) (string, error) {
-	// 1. 构造请求
-	req := ChatGPTRequest{
-		Prompt: prompt,
-	}
+func (c *ChatGPT) request(req ChatGPTRequest) (*ChatGPTResponse, error) {
 	reqBytes, err := json.Marshal(req)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-
 	// 2. 发送请求
 	url := "https://api.openai.com/v1/engines/davinci/completions"
 	reqReader := bytes.NewReader(reqBytes)
 	httpReq, err := http.NewRequest("POST", url, reqReader)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	httpReq.Header.Set("Content-Type", "application/json")
 	httpReq.Header.Set("Authorization", "Bearer "+c.APIKey)
 	httpClient := http.Client{}
 	resp, err := httpClient.Do(httpReq)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	// 3. 解析响应
 	respBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	var respData ChatGPTResponse
 	err = json.Unmarshal(respBytes, &respData)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return respData.Choices[0].Text, nil
+	return &respData, nil
+	// return respData.Choices[0].Text, nil
 }
