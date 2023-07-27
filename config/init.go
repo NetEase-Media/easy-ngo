e1c9ce8c7199cced69ade0e6353c092ac8c50561
chaikebin <chkeb@126.com>
2023-07-27 19:50:28 +0800

    delete config

--- config/init.go
+++
@@ -1,7 +0,0 @@
-package config
-
-// import "github.com/NetEase-Media/easy-ngo/config/contrib/env"
-
-// func init() {
-// 	Register(env.EnvConfigSourceName, env.New())
-// }
--- config/protocol.go
+++
@@ -1,4 +0,0 @@
-package config
-
-type Protocol struct {
-}
