b922b69de87db230fbd2ecf16f2f6eec13fb2736
Darcy <tanoya321@163.com>
2023-12-11 10:43:09 +0800

    perfection test case for promethus
    Signed-off-by: Darcy <tanoya321@163.com>

--- xmetrics/contrib/xprometheus/xprometheus_test.go
+++ xmetrics/contrib/xprometheus/xprometheus_test.go
@@ -83,3 +83,97 @@ func TestGauge(t *testing.T) {
 	m.Write(&data)
 	assert.Equal(t, 5.0, *data.Gauge.Value)
 }
+
+func TestSummary(t *testing.T) {
+	// with label
+	s := NewSummaryFrom(prometheus.SummaryOpts{
+		Namespace:  "",
+		Subsystem:  "",
+		Name:       "http_requests_duration",
+		Help:       "http_requests_duration",
+		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+	}, []string{"method", "code"})
+
+	s.With("method", "GET", "code", "200").Observe(2)
+	value := make(chan prometheus.Metric, 1)
+	s.sv.Collect(value)
+	m := <-value
+	data := dto.Metric{}
+	m.Write(&data)
+	assert.Equal(t, 2.0, *data.Summary.SampleSum)
+	assert.Equal(t, uint64(1), *data.Summary.SampleCount)
+
+	s.With("method", "GET", "code", "200").Observe(3)
+	value = make(chan prometheus.Metric, 1)
+	s.sv.Collect(value)
+	m = <-value
+	data = dto.Metric{}
+	m.Write(&data)
+	assert.Equal(t, 5.0, *data.Summary.SampleSum)
+	assert.Equal(t, uint64(2), *data.Summary.SampleCount)
+
+	// without label
+	s2 := NewSummaryFrom(prometheus.SummaryOpts{
+		Namespace:  "",
+		Subsystem:  "",
+		Name:       "redis_requests_total",
+		Help:       "redis_requests_total",
+		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+	}, nil)
+
+	s2.Observe(2)
+	value = make(chan prometheus.Metric, 1)
+	s2.sv.Collect(value)
+	m = <-value
+	data = dto.Metric{}
+	m.Write(&data)
+	assert.Equal(t, 2.0, *data.Summary.SampleSum)
+	assert.Equal(t, uint64(1), *data.Summary.SampleCount)
+}
+
+func TestHistogram(t *testing.T) {
+	// with label
+	h := NewHistogramFrom(prometheus.HistogramOpts{
+		Namespace: "",
+		Subsystem: "",
+		Name:      "http_requests_duration",
+		Help:      "http_requests_duration",
+		Buckets:   []float64{10, 200, 400, 600, 1000},
+	}, []string{"method", "code"})
+
+	h.With("method", "GET", "code", "200").Observe(100)
+	value := make(chan prometheus.Metric, 1)
+	h.hv.Collect(value)
+	m := <-value
+	data := dto.Metric{}
+	m.Write(&data)
+	assert.Equal(t, 100.0, *data.Histogram.SampleSum)
+	assert.Equal(t, uint64(1), *data.Histogram.SampleCount)
+
+	h.With("method", "GET", "code", "200").Observe(200)
+	value = make(chan prometheus.Metric, 1)
+	h.hv.Collect(value)
+	m = <-value
+	data = dto.Metric{}
+	m.Write(&data)
+	assert.Equal(t, 300.0, *data.Histogram.SampleSum)
+	assert.Equal(t, uint64(2), *data.Histogram.SampleCount)
+
+	// without label
+	h2 := NewHistogramFrom(prometheus.HistogramOpts{
+		Namespace: "",
+		Subsystem: "",
+		Name:      "redis_requests_total",
+		Help:      "redis_requests_total",
+		Buckets:   []float64{10, 200, 400, 600, 1000},
+	}, nil)
+
+	h2.Observe(100)
+	value = make(chan prometheus.Metric, 1)
+	h2.hv.Collect(value)
+	m = <-value
+	data = dto.Metric{}
+	m.Write(&data)
+	assert.Equal(t, 100.0, *data.Histogram.SampleSum)
+	assert.Equal(t, uint64(1), *data.Histogram.SampleCount)
+}
